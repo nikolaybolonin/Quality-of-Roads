69d93a96a38c05ef5155ab6fec288901f8acfca2
Glider-of-chaos <g4regtrash@gmail.com>
2017-01-12 23:13:33 +0300

    I think that's it for prototype db.

--- src/php/db_functions.php
+++ src/php/db_functions.php
@@ -55,7 +55,6 @@ function add_node($connection, $node_coords, $timestamp = NULL, $parent = NULL) 
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-
 }
 
 function add_line($connection, $start_node_id, $end_node_id, $timestamp = NULL, $parent = NULL) {
@@ -67,7 +66,6 @@ function add_line($connection, $start_node_id, $end_node_id, $timestamp = NULL, 
     } else {
         echo "Error: " . $sql . "<br>" . $conn->error;
     }
-
 }
 
 function show_json($json) {
@@ -146,7 +144,7 @@ function create_tables($conn) {
     $sql ="CREATE TABLE Pavement_translation (
         pavement_translation_id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         pavement_id INT(2) UNSIGNED,
-        language_id INT(3) UNSIGNED
+        language_id INT(3) UNSIGNED,
         pavement_name VARCHAR(30) NOT NULL,
         pavement_descr VARCHAR(100))";
 
@@ -156,7 +154,7 @@ function create_tables($conn) {
         echo "error creating table: " . $conn->error;
     }
 
-    echo "creating Languagestable";
+    echo "creating Languages table";
     $sql ="CREATE TABLE Languages (
         language_id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
         language_en_name VARCHAR(30) NOT NULL,
@@ -169,21 +167,100 @@ function create_tables($conn) {
         echo "error creating table: " . $conn->error;
     }
 
-    //and to insert default pavement type (TODO: make this in a normal way)
-    $sql_i = "INSERT INTO map_01.pavement (id, pavement_name, pavement_descr) VALUES (1, 'tiles', 'tiles description');";
-    if ($conn->query($sql_i) === TRUE) {
-        echo "<br>def. pavement inserted<br>";
+    echo "creating Quality table";
+    $sql ="CREATE TABLE Quality (
+        quality_id INT(2) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        quality_value INT(2) UNSIGNED,
+        quality_color_hex VARCHAR(6))";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "table quality created successfully";
     } else {
         echo "error creating table: " . $conn->error;
     }
-/*
-    $conn2 = $conn;
 
-    set_line_uncrowded($conn2, 1, 8);
-    $conn2 = $conn;
+    echo "creating Quality translation table";
+    $sql ="CREATE TABLE Quality_translation (
+        quality_translation_id INT(5) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+        quality_id INT(1) UNSIGNED,
+        language_id INT(3) UNSIGNED,
+        quality_descr VARCHAR(100))";
 
-    set_line_surface_quality($conn2, 1, 7);
- */
+    if ($conn->query($sql) === TRUE) {
+        echo "table quality translations created successfully";
+    } else {
+        echo "error creating table: " . $conn->error;
+    }
+}
+
+// Half-assed function to get some basic data in for future testing. TODO: get some proper basic data upload functionality)
+function upload_base_data($connection) {
+
+    echo "<br>== Inserting base data ==<br>";
+
+    echo "<br>pavement<br>";
+    $sql_i = "INSERT INTO map_01.pavement (pavement_id) VALUES (1), (2);";
+    if ($connection->query($sql_i) === TRUE) {
+        echo "<br>def. pavement ids inserted<br>";
+    } else {
+        echo "error creating table: " . $connection->error;
+    }
+
+    echo "<br>Languages<br>";
+    $sql_i = "INSERT INTO map_01.languages (language_en_short_name, language_en_name, language_local_name)
+                                    VALUES ('EN', 'English', 'English'),
+                                            ('RU', 'Russian', 'Русский');";
+    if ($connection->query($sql_i) === TRUE) {
+        echo "<br>languages inserted<br>";
+    } else {
+        echo "error creating table: " . $connection->error;
+    }
+
+    echo "<br>pavement translation<br>";
+    $sql_i = "INSERT INTO map_01.pavement_translation (pavement_id, language_id, pavement_name, pavement_descr)
+                                    VALUES (1, 1, 'asphalt', 'fairly smooth but can have cracks and may include gravel'),
+                                            (1, 2, 'асфальт', 'сравнительно ровная поверхность, возможны трещины и может включать в себя гравий'),
+                                            (2, 1, 'tiles', 'small beveled tiles, cracks are imminent, really uncomfortable for small wheels'),
+                                            (2, 2, 'плитка', 'мелкая неровная плитка, постоянные грани, дискофортно для маленький колес');";
+    if ($connection->query($sql_i) === TRUE) {
+        echo "<br>def. pavement translation inserted<br>";
+    } else {
+        echo "error creating table: " . $connection->error;
+    }
+
+    echo "<br>quality<br>";
+    $sql_i = "INSERT INTO map_01.quality (quality_value, quality_color_hex)
+                VALUES (0, '000000'),
+                        (1, 'ac1417'),
+                        (2, 'dc7519'),
+                        (3, 'cbc324'),
+                        (4, 'afeb20'),
+                        (5, '26b62b');";
+    if ($connection->query($sql_i) === TRUE) {
+        echo "<br>qualities inserted<br>";
+    } else {
+        echo "error creating table: " . $connection->error;
+    }
+
+    echo "<br>quality translation<br>";
+    $sql_i = "INSERT INTO map_01.quality_translation (quality_id, language_id, quality_descr)
+                VALUES (1, 1, 'impossible to ride'),
+                        (2, 1, 'possible to ride but really risky'),
+                        (3, 1, 'really uncomfortable'),
+                        (4, 1, 'uncomfortable'),
+                        (5, 1, 'good with minor irritation'),
+                        (6, 1, 'good'),
+                        (1, 1, 'ехать невозможно'),
+                        (2, 1, 'ехать можно, но рискованно'),
+                        (3, 1, 'ехать очень неприятно'),
+                        (4, 1, 'ехать неприятно'),
+                        (5, 1, 'хорошо, но с мелкими раздражающими факторами'),
+                        (6, 1, 'хорошо');";
+    if ($connection->query($sql_i) === TRUE) {
+        echo "<br>quality translations inserted<br>";
+    } else {
+        echo "error creating table: " . $connection->error;
+    }
 }
 
 function find_or_add_node($connection, $node_coords, $timestamp, $parent) {
